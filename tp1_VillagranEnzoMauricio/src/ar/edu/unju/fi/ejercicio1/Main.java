ef6a8f8da5c8f2cbc923d5868db82060b2265eee
EnzoVilla <enzovilla1810@gmail.com>
2024-04-03 20:10:31 -0300

    desarrollo tp1 ejercicio1

---
+++ tp1_VillagranEnzoMauricio/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int numA=8,numB=5,numC=1,numD=20,numE=9;
+		double totalPromedio = (numA+numB+numC+numD+numE)/5.;
+		System.out.println("El promedio es: "+totalPromedio);
+
+	}
+
+}
