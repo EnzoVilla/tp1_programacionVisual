ff5442a08cfac524a9bce2d600955da534a30908
EnzoVilla <enzovilla1810@gmail.com>
2024-04-09 00:25:13 -0300

    Desarrollo del ejercicio 2 y corrección del ejercicio 1 linea 7 por conversion explicita al tipo float

--- tp1_VillagranEnzoMauricio/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_VillagranEnzoMauricio/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		int numA=8,numB=5,numC=1,numD=20,numE=9;
-		double totalPromedio = (numA+numB+numC+numD+numE)/5.;
+		float totalPromedio = (numA+numB+numC+numD+numE)/5.0f;
 		System.out.println("El promedio es: "+totalPromedio);
 
 	}
---
+++ tp1_VillagranEnzoMauricio/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nombrePais = "Argentina";
+		byte aniosPersona = 22;
+		float alturaEdificio = 40.67f;
+		float precio = 12.32f;
+		long telefono = 5444883439614l;
+		double coseno = 0.87758256189037;
+		System.out.println(nombrePais);
+		System.out.println(aniosPersona);
+		System.out.println(alturaEdificio);
+		System.out.println(precio);
+		System.out.println(telefono);
+		System.out.println(coseno);
+		
+	}
+
+}
